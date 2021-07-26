97b03a796b233624d1ec900a7189c31b5bfed96c
Dmytro Salii <dmitriysalii@gmail.com>
2021-07-26 17:56:29 +0300

    pre-final version

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import menuMarkUpTemplate from './templates/menu.hbs';
 
 
 const menuRef = document.querySelector('.js-menu');
+const checkBoxRef = document.querySelector('.theme-switch__toggle');
+const bodyRef = document.querySelector('body');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 
 
@@ -11,6 +17,39 @@ function createMenuMarkUp(List) {
     return List.map(menuMarkUpTemplate).join('');
 };
 
-
 menuRef.insertAdjacentHTML('afterbegin', createMenuMarkUp(menuList));
+
+    localStorage.getItem("Theme") === 'light-theme'
+  ? (checkBoxRef.checked = false)
+        : (checkBoxRef.checked = true);
+    bodyRef.classList.add(localStorage.getItem("Theme")); 
+
+
+
+
+// Change theme theme light
+checkBoxRef.addEventListener('click', onPressCheckBox);
+
+function onPressCheckBox(event) {
+  
+    if (localStorage.getItem("Theme") === "light-theme") {
+        bodyRef.classList.remove(localStorage.getItem("Theme"));
+        localStorage.clear();
+        localStorage.setItem("Theme", "dark-theme");
+      
+        
+        
+    } else {
+        bodyRef.classList.remove(localStorage.getItem("Theme"));
+        localStorage.clear();
+        localStorage.setItem("Theme", "light-theme");
+        
+    };
+   bodyRef.classList.add(localStorage.getItem("Theme"));
+   
+}
+
+
+
+
 
