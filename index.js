6a6d89c21cfb888949f4190dac1cdc2571534add
Dmytro Salii <dmitriysalii@gmail.com>
2021-08-06 09:33:24 +0300

    fin

--- index.js
+++ index.js
@@ -5,43 +5,33 @@ import menuMarkUpTemplate from './templates/menu.hbs';
 const menuRef = document.querySelector('.js-menu');
 const checkBoxRef = document.querySelector('.theme-switch__toggle');
 const bodyRef = document.querySelector('body');
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
 
 
 
 function createMenuMarkUp(List) {
     return List.map(menuMarkUpTemplate).join('');
 };
-
 menuRef.insertAdjacentHTML('afterbegin', createMenuMarkUp(menuList));
 
-//     localStorage.getItem("Theme") === 'light-theme'
-//   ? (checkBoxRef.checked = false)
-//         : (checkBoxRef.checked = true);
+
 
 
 // Change theme theme light
 checkBoxRef.addEventListener('change', onPressCheckBox);
 
+
+
 function onPressCheckBox(event) {
-  
-    if (localStorage.getItem("Theme") === "light-theme") {
-        bodyRef.classList.remove(localStorage.getItem("Theme"));
-        localStorage.clear();
-        checkBoxRef.checked = true;
+    if (event.target.checked) {
+        
         localStorage.setItem("Theme", "dark-theme");
-      
-        
-        
+        bodyRef.classList.replace("light-theme", "dark-theme");
+             
     } else {
-        bodyRef.classList.remove(localStorage.getItem("Theme"));
-        localStorage.clear();
         checkBoxRef.checked = false;
+        localStorage.removeItem("Theme");
         localStorage.setItem("Theme", "light-theme");
+        bodyRef.classList.replace("dark-theme", "light-theme");
         
     };
    bodyRef.classList.add(localStorage.getItem("Theme"));
@@ -49,9 +39,14 @@ function onPressCheckBox(event) {
 }
 
 
-localStorage.getItem("Theme") === 'dark-theme'
+    localStorage.getItem("Theme") === 'dark-theme'
     ? (checkBoxRef.checked = true) : (checkBoxRef.checked = false);
-     bodyRef.classList.add(localStorage.getItem("Theme"));
+   bodyRef.classList.add(localStorage.getItem("Theme"));
+
+
+
+
+
 
 
 
